1f7f6001b1452c0a55a834efbc3780e752c759e0
Stimonis <simon.stiens@gmail.com>
2018-04-14 11:12:19 +0200

    add

--- fanPage1/galleryScript.js
+++
@@ -1,7 +0,0 @@
-$(".photo").click(function() {
-  let photo = $(this).attr('data-src');
-  let taille = this.width + 50;
-  console.log(this.width);
-  $(".modal-content").css("width", taille);
-  $(".modal-body").html("<img class=photoMod src=" + photo + "> </img>");
-});
